195c450f9b10bfa334b7ca7315a4d29dda87905a
JB desarrollo <ichiparanoid@gmail.com>
2022-12-05 03:08:21 +0000

    Update Menu.jsx

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,15 +1,13 @@
 import React, { Suspense, useRef, useState } from "react";
-import { Canvas, useLoader } from "@react-three/fiber";
-import { GLTFLoader } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import Bad from "./Bad.jsx";
 
 const Menu = () => {
-  const gltf = useLoader(GLTFLoader, "../Modelos/Bad.glb");
-
   return (
     <>
       <Canvas camera={{ fov: 75, position: [0, 2, 5] }}>
         <Suspense fallback={null}>
-          <primitive object={gltf.scene} />
+          <Bad />
         </Suspense>
       </Canvas>
     </>
